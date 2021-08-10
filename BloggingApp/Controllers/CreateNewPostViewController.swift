26d002919f6f6394df5c341396d96dd57eb05814
elina-mns <ellie.mansurova@gmail.com>
2021-08-10 15:40:54 -0400

    configure nav buttons for create new post vc

--- BloggingApp/Controllers/CreateNewPostViewController.swift
+++ BloggingApp/Controllers/CreateNewPostViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class CreateNewPostViewController: UITabBarController {
+    
+    //Title field
+    //Image Header
+    //TextView for the post
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        configureButtons()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureButtons() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(didTapCancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post!", style: .done, target: self, action: #selector(didTapPost))
     }
-    */
+    
+    @objc func didTapCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func didTapPost() {
+        
+    }
 
 }
