515df4bf2e592ecfcf6c0300f4635c1e206090e1
elina-mns <ellie.mansurova@gmail.com>
2021-08-11 12:41:42 -0400

    add image picker controller, create a post, configure a tap gesture

--- BloggingApp/Controllers/CreateNewPostViewController.swift
+++ BloggingApp/Controllers/CreateNewPostViewController.swift
@@ -9,8 +9,9 @@ import UIKit
 
 class CreateNewPostViewController: UITabBarController {
     
-    //Title field
+    //MARK: Properties
     
+    //Title field
     private let titleField: UITextField = {
        let field = UITextField()
         field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
@@ -25,10 +26,9 @@ class CreateNewPostViewController: UITabBarController {
     }()
     
     //Image Header
-    
     private var headerImageView: UIImageView = {
         let headerImageView = UIImageView()
-        headerImageView.contentMode = .center
+        headerImageView.contentMode = .scaleToFill
         headerImageView.isUserInteractionEnabled = true
         headerImageView.image = UIImage(systemName: "photo")
         headerImageView.tintColor = .darkGray
@@ -37,8 +37,9 @@ class CreateNewPostViewController: UITabBarController {
         return headerImageView
     }()
     
-    //TextView for the post
+    private var selectedHeaderImage: UIImage!
     
+    //TextView for the post
     private var contentTextView: UITextView = {
         let textView = UITextView()
         textView.isEditable = true
@@ -57,13 +58,14 @@ class CreateNewPostViewController: UITabBarController {
         view.addSubview(headerImageView)
         view.addSubview(contentTextView)
         configureButtons()
+        configureTapGesture()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         titleField.frame = CGRect(x: 10, y: view.safeAreaInsets.top + 20, width: view.width - 20, height: 50)
-        headerImageView.frame = CGRect(x: 30, y: titleField.bottom + 20, width: view.width - 70, height: 200)
-        contentTextView.frame = CGRect(x: 20, y: headerImageView.bottom + 20, width: view.width - 40, height: view.height - 210 - view.safeAreaInsets.top - 50)
+        headerImageView.frame = CGRect(x: 30, y: titleField.bottom + 30, width: view.width - 60, height: 200)
+        contentTextView.frame = CGRect(x: 20, y: headerImageView.bottom + 20, width: view.width - 40, height: 400)
     }
     
     func configureButtons() {
@@ -71,12 +73,51 @@ class CreateNewPostViewController: UITabBarController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post!", style: .done, target: self, action: #selector(didTapPost))
     }
     
+    func configureTapGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapHeaderImage))
+        headerImageView.addGestureRecognizer(tap)
+    }
+    
+    //MARK: Objc methods
+    
     @objc func didTapCancel() {
         dismiss(animated: true, completion: nil)
     }
     
-    @objc func didTapPost() {
-        
+    @objc func didTapHeaderImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
     }
+    
+    @objc func didTapPost() {
+        guard let title = titleField.text,
+              let body = contentTextView.text,
+              let image = selectedHeaderImage,
+              !title.trimmingCharacters(in: .whitespaces).isEmpty,
+              !body.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+        
+        //Upload header image
+        //Insert post into DB
+        
+        let post = BlogPost(id: UUID().uuidString, title: title, timestamp: Date().timeIntervalSince1970, headerImageURL: nil, text: body)
+    }
+    
+}
 
+    //MARK: Image Picker Controller methods
+
+extension CreateNewPostViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[.originalImage] as? UIImage else { return }
+        selectedHeaderImage = image
+        headerImageView.image = image
+    }
 }
